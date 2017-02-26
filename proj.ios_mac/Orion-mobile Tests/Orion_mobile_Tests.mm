1acd9e3ea5b5afe32c20597afd1d3bad1ee9a06c
RevLow <rev_low@yahoo.co.jp>
2017-02-26 20:02:21 +0900

    [mod] Movie#addFrameのテストを追加

--- proj.ios_mac/Orion-mobile Tests/Orion_mobile_Tests.mm
+++ proj.ios_mac/Orion-mobile Tests/Orion_mobile_Tests.mm
@@ -13,8 +13,10 @@
  privateメソッドのaddFrameを呼び出すために定義
 */
 struct MovieMakeFrame { typedef Orion::MovieContext::Instruction (Orion::MovieContext::Movie::* type)(const rapidjson::Value&); };
+struct MovieFrames { typedef std::vector<Orion::MovieContext::Frame> Orion::MovieContext::Movie::* type; };
 
 template struct Initializer<MovieMakeFrame, &Orion::MovieContext::Movie::makeFrameInstruction>;
+template struct Initializer<MovieFrames, &Orion::MovieContext::Movie::_frames>;
 
 @interface Orion_mobile_Tests : XCTestCase
 
@@ -34,7 +36,7 @@ template struct Initializer<MovieMakeFrame, &Orion::MovieContext::Movie::makeFra
     [super tearDown];
 }
 
-#pragma mark - @describe Instructionの検証
+#pragma mark - @describe Movie#makeFrameInstructionの検証
 #pragma mark RemoveObject
 /*
    @context valueのtypeがRemoveObjectの場合
@@ -182,6 +184,10 @@ template struct Initializer<MovieMakeFrame, &Orion::MovieContext::Movie::makeFra
 
 #pragma mark PlaySound
 
+/*
+    @context valueのtypeがPlaySoundの場合
+    @it      type, soundIdが入力したjsonデータに応じた結果になる
+ */
 -(void)testPlaySound
 {
     Orion::MovieContext::Movie movie;
@@ -193,6 +199,95 @@ template struct Initializer<MovieMakeFrame, &Orion::MovieContext::Movie::makeFra
     auto instruction = (movie.*Accessor<MovieMakeFrame>::value)(s);
     XCTAssertEqual(instruction.type, Orion::MovieContext::Instruction::InstructionType::PLAY_SOUND, @"Type is PlaySound");
     XCTAssertEqual(instruction.soundId, 1, @"Sound id is 1");
+}
+
+#pragma mark - @describe Movie#addFrameの検証
+
+/*
+    @context movieIdが存在するinstructionを含んでいる場合
+    @it      frameIdとinstructionsがjsonに応じた結果になる
+*/
+-(void)testAddFrame
+{
+    Orion::MovieContext::Movie movie;
+    const char* json = R"({"frames":
+      [
+        {"frameId":1, "instructions":[
+            {"type":"PlaceObject", "layer":1, "movieId":3,"transform":{"a":1,"b":0,"c":0,"d":1,"tx":0,"ty":0}},
+            {"type":"NextFrame"}]
+        }
+      ]
+    })";
+    rapidjson::Document d;
+    d.Parse(json);
+    rapidjson::Value& s = d["frames"];
+
+    movie.addFrame(s);
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[0].frameId, 1, @"frameId is 1");
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[0].instructions[0].type, Orion::MovieContext::Instruction::InstructionType::PLACE_OBJECT, @"0 instruction type is PlaceObject");
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[0].instructions[1].type, Orion::MovieContext::Instruction::InstructionType::NEXT_FRAME, @"1 instruction type is NextFrame");
+}
+
+/*
+    @context movieIdが存在しないinstructionを含んでいる場合
+    @it      instructionのmovieIdが既に存在するmovieIdで埋められている
+ */
+-(void)testAddFrameNonMovieId
+{
+    Orion::MovieContext::Movie movie;
+    Orion::MovieContext::Frame frame;
+    frame.frameId = 1;
+    Orion::MovieContext::Instruction instruction;
+    instruction.type = Orion::MovieContext::Instruction::InstructionType::PLACE_OBJECT;
+    instruction.layer = 1;
+    instruction.movieId = 3;
+    frame.instructions.emplace_back(instruction);
+    (movie.*Accessor<MovieFrames>::value).emplace_back(frame);
+
+    const char* json = R"({"frames":
+    [
+     {"frameId":2, "instructions":[
+                                   {"type":"PlaceObject", "layer":1, "transform":{"a":1,"b":0,"c":0,"d":1,"tx":0,"ty":0}},
+                                   {"type":"NextFrame"}]
+     }
+    ]
+    })";
+    rapidjson::Document d;
+    d.Parse(json);
+    rapidjson::Value& s = d["frames"];
+
+    movie.addFrame(s);
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[1].frameId, 2, @"frameId is 2");
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[1].instructions[0].type, Orion::MovieContext::Instruction::InstructionType::PLACE_OBJECT, @"0 instruction type is PlaceObject");
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[1].instructions[0].movieId, 3, @"instruction movieId interpolated");
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[1].instructions[1].type, Orion::MovieContext::Instruction::InstructionType::NEXT_FRAME, @"1 instruction type is NextFrame");
+
+}
+
+/*
+    @context labelが存在するFrameの場合
+    @it      Frameのラベルがjsonで指定したラベルになっている
+ */
+-(void)testAddFrameWithLabel
+{
+    Orion::MovieContext::Movie movie;
+    const char* json = R"({"frames":
+    [
+     {"frameId":1, "label": "label", "instructions":[
+                                                     {"type":"PlaceObject", "layer":1, "movieId":3,"transform":{"a":1,"b":0,"c":0,"d":1,"tx":0,"ty":0}},
+                                                     {"type":"NextFrame"}]
+     }
+    ]
+    })";
+    rapidjson::Document d;
+    d.Parse(json);
+    rapidjson::Value& s = d["frames"];
+
+    movie.addFrame(s);
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[0].frameId, 1, @"frameId is 1");
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[0].instructions[0].type, Orion::MovieContext::Instruction::InstructionType::PLACE_OBJECT, @"0 instruction type is PlaceObject");
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[0].instructions[1].type, Orion::MovieContext::Instruction::InstructionType::   NEXT_FRAME, @"1 instruction type is NextFrame");
+    XCTAssertEqual((movie.*Accessor<MovieFrames>::value)[0].label, "label", @"label is label");
 }
 
 @end
